9a8145d619633a774b491877ba78141dc6db83fc
Tiafeno <tiafenofnel@gmail.com>
2017-07-29 23:20:41 +0300

    Implement meta box for page type post
    * Verification function
    * Add Meta box content type
    * Add render method

--- dc.php
+++ dc.php
@@ -69,29 +69,50 @@
   }
 
   public function addMetaBox(){
-    add_meta_box('favorite_works', 'Favorite Works', 
-    array($this, 'render_meta_box'), 
-    [
+    $TypeContents = [
       '360deg', 'digital', 'marketing', 'advertising',
       'edition', 'packaging', 'branding', 'event', 'store_booth'
-    ], 'side', 'high');
+    ];
+
+    /*
+    * Add meta box from all Type content in $this->PostType
+    */
+    add_meta_box('favorite_works', 'Favorite Works', array($this, 'render_meta_box_fw'), $TypeContents, 'side', 'high');
+    /*
+    * Add meta box in post type content
+    */
+    add_meta_box( 'content_type', 'Content Post Type', array($this, 'render_meta_box_posttype'), 
+      [
+        'page'
+      ], 'side', 'high' );
   }
 
-  public function action_save_postdata($post_id){
-    if (!isset($_POST[ 'fw_nonce' ] ) || !wp_verify_nonce( $_POST[ 'fw_nonce' ],'fw_metabox_nonce') ) 
-      return;
-
-    if ((defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE)
-      || (defined( 'DOING_AJAX' ) && DOING_AJAX)
-    ) return;
-
+  private function verification(){
+    if ((defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE) || (defined( 'DOING_AJAX' ) && DOING_AJAX)) 
+      return false;
     if (!current_user_can( 'edit_posts' ))
-      return;
-    $value = (isset($_POST[ 'favorite_works' ])) ? trim($_POST[ 'favorite_works' ]) : 0;
-    update_post_meta($post_id, 'favorite_works', $value);
+      return false;
+    return true;
   }
 
-  public function render_meta_box($post){
+  public function action_save_postdata( $post_id ){
+    if (isset($_POST[ 'favorite_works' ])){
+      if (!isset($_POST[ 'fw_nonce' ] ) || !wp_verify_nonce( $_POST[ 'fw_nonce' ], 'fw_metabox_nonce') ) 
+        return;
+      if (!$this->verification()) return;
+      $value = (isset($_POST[ 'favorite_works' ])) ? trim($_POST[ 'favorite_works' ]) : 0;
+      update_post_meta($post_id, 'favorite_works', $value);
+    }
+
+    if (isset($_POST[ 'content_type' ])){
+      if (!$this->verification()) return;
+      $ContentValue = (isset($_POST[ 'content_type' ])) ? trim($_POST[ 'content_type' ]) : 0;
+      update_post_meta( $post_id, 'content_type', $ContentValue);
+    }
+    
+  }
+
+  public function render_meta_box_fw( $post ){
     $favorite_works_value = get_post_meta($post->ID, 'favorite_works', true);
     ?>
       <section>
@@ -102,6 +123,31 @@
           Favorite Works
         </label>
       </section>
+    <?php
+  }
+
+  public function render_meta_box_posttype( $post ){
+    $content_type = get_post_meta($post->ID, 'content_type', true);
+    if (is_array($this->PostTypes) && empty($this->PostTypes)) return false;
+    ?>
+     <fieldset>
+        <input type="radio" name="content_type" class="post-format" value="0" 
+         <?= (empty( $content_type )) ? ' checked="checked" ': '' ?>
+         id="content-type-0"></input>
+        <label for="content-type-0" class="post-format-icon">
+          DEFAULT
+        </label>
+        <br>
+     <?php foreach ($this->PostTypes as $posttype): ?>
+        <input type="radio" name="content_type" class="post-format" value="<?= $posttype[ 'type' ] ?>" 
+          <?= (!empty( $content_type ) && $content_type == $posttype[ 'type' ]) ? ' checked="checked" ': '' ?>
+          id="<?= 'content-type-'.$posttype[ 'type' ] ?>"></input>
+        <label for="<?= 'content-type-'.$posttype[ 'type' ] ?>" class="post-format-icon">
+          <?= strtoupper($posttype[ 'label' ]) ?>
+        </label>
+        <br>
+      <?php endforeach; ?>
+      </fieldset>
     <?php
   }
 
